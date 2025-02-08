842c25c53ef5afc1aa79943729a103b536e20b2f
Andrew Lam <lam00139@morris.umn.edu>
2025-02-08 17:34:47 -0600

    Add experience, data file for experience and skills, icon for skills and platform

--- Andrew-Lam-Portfolio/src/App.jsx
+++ Andrew-Lam-Portfolio/src/App.jsx
@@ -2,6 +2,7 @@ import styles from "./App.module.css";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Hero } from "./components/Navbar/Hero/Hero";
 import { About } from "./components/Navbar/About/About";
+import { Experience } from "./components/Navbar/Experience/Experience";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Experience />
     </div>
   );
 }
