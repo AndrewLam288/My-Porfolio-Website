4563a20cf1d37f6e32ef897667d4d206faf57175
Andrew Lam <lam00139@morris.umn.edu>
2025-02-08 20:50:32 -0600

    Fix overflow in app.jsx, Add Projects section and ProjectCard, json

--- Andrew-Lam-Portfolio/src/App.jsx
+++ Andrew-Lam-Portfolio/src/App.jsx
@@ -3,6 +3,7 @@ import { Navbar } from "./components/Navbar/Navbar";
 import { Hero } from "./components/Navbar/Hero/Hero";
 import { About } from "./components/Navbar/About/About";
 import { Experience } from "./components/Navbar/Experience/Experience";
+import { Projects } from "./components/Navbar/Projects/Projects";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <About />
       <Experience />
+      <Projects />
     </div>
   );
 }
